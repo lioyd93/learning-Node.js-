d8fe926b98996a0f0756104fe831e83e8defda6e
loay <loaymannaa@outlook.com>
2020-12-13 17:45:51 +0100

    submit secrets

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const userSchema= new mongoose.Schema({
   email: String,
   password: String,
   googleID: String,
-  secret: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -97,11 +97,50 @@ app.get("/register", function(req,res){
 
 
 app.get("/secrets",function(req,res){
+User.find({"secret": {$ne:null}
+
+},function(err,foundUser){
+  if(err)
+  {
+    console.log(err);
+
+  }
+  else{
+    if(foundUser){
+      res.render("secrets",{usersWithSecrets: foundUser})
+    }
+
+  }
+});
+});
+
+
+
+app.get("/submit",function(req,res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
+});
+
+app.post("/submit",function(req,res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret=submittedSecret;
+        foundUser.save(function()
+        {
+        res.redirect("/secrets");
+      });
+      }
+    }
+  });
 });
 
 
